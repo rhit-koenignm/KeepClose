d799bbdbbc5f263fed72438ab2ea6cefbb8c24e4
ChronicApp <koenignm@rose-hulman.edu>
2023-04-26 17:38:06 -0400

    Pushing initial flutter project

---
+++ keep_close/android/app/src/main/kotlin/rose/hulman/koenignm/keep_close/MainActivity.kt
@@ -0,0 +1,6 @@
+package rose.hulman.koenignm.keep_close
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ keep_close/lib/components/keep_close_color_theme.dart
@@ -0,0 +1,37 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class KCColorTheme {
+
+  //************ Start of Color Palette Functions ************
+
+  static Color getSeasalt() {
+    return Color.fromRGBO(248, 247, 249, 1.0);
+  }
+
+  static Color getVistaBlue() {
+    return Color.fromRGBO(142, 162, 255, 1.0);
+  }
+  
+  static Color getBittersweet() {
+    return Color.fromRGBO(228, 112, 104, 1.0);
+  }
+
+  static Color getHunyadiYellow() {
+    return Color.fromRGBO(250, 177, 65, 1.0);
+  }
+
+  //~~~~~~~~~~~~ Status Message Colors ~~~~~~~~~~~~
+
+  static Color getGreen(){
+    return Color.fromRGBO(88, 129, 87, 1.0);
+  }
+
+  static Color getGray() {
+    return Color.fromRGBO(162, 156, 157, 1.0);
+  }
+
+  static Color getRed() {
+    return Color.fromRGBO(255, 0, 0, 1.0);
+  }
+}
